50a30681197fc77569ace82d76af20c40c138cb6
Santicm23 <santicastrom@outlook.com>
2025-10-28 13:29:21 -0500

    moving inventory files and creating ansible installation bash script

---
+++ installation.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+sudo apt update
+# Install Python 3 and pip
+sudo apt install python3
+sudo apt install python3-pip
+
+# Install pipx
+sudo apt install pipx
+pipx ensurepath
+
+# Install Ansible
+pipx install --include-deps ansible
+
+# Install AWS CLI
+curl "https://awscli.amazonaws.com/awscli-exe-linux-x86_64.zip" -o "awscliv2.zip"
+unzip awscliv2.zip
+sudo ./aws/install --bin-dir /usr/local/bin --install-dir /usr/local/aws-cli --update
