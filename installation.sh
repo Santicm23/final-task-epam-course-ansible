6d0434f3c9d9658c87300b3328ecb53c96a577d7
Santicm23 <santicastrom@outlook.com>
2025-10-28 14:10:54 -0500

    fixing inventory files name

--- installation.sh
+++ installation.sh
@@ -11,8 +11,12 @@ pipx ensurepath
 
 # Install Ansible
 pipx install --include-deps ansible
+pipx ensurepath
+source ~/.bashrc
+pipx inject ansible boto3 botocore
 
 # Install AWS CLI
+sudo apt install unzip
 curl "https://awscli.amazonaws.com/awscli-exe-linux-x86_64.zip" -o "awscliv2.zip"
 unzip awscliv2.zip
 sudo ./aws/install --bin-dir /usr/local/bin --install-dir /usr/local/aws-cli --update
